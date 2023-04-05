6d92b182edc6c36f1f0e011fcd70cf38509a9c1b
ItZxSTaR / <117068254+ItZxSTaR@users.noreply.github.com>
2023-04-05 13:05:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 from os import getenv
-from dotenv import load_dotenv
 
-load_dotenv()
 que = {}
 admins = {}
 
@@ -9,9 +7,9 @@ API_ID = int(getenv("API_ID", "4110592"))
 API_HASH = getenv("API_HASH", "aa7c849566922168031b95212860ede0")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 BOT_NAME = getenv("BOT_NAME","ғᴀʟʟᴇɴ ᴍᴜsɪᴄ ʙᴏᴛ")
-BOT_USERNAME = getenv("BOT_USERNAME", "fallen_music_bot")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "anonymous_was_bot")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "DevilsHeavenMF")
+BOT_USERNAME = getenv("BOT_USERNAME", "")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "PyXen")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "AltronChats")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/e22fb003bdbb83a96288c.jpg")
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/e22fb003bdbb83a96288c.jpg")
