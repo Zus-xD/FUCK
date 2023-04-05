60c089622b68a81c84059a00ed4647e359e0c563
Zus-xD <126873958+Zus-xD@users.noreply.github.com>
2023-04-05 12:42:52 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from helpers.filters import command
-from config import BOT_NAME as bn, BOT_USERNAME as bu, START_IMG
+from config import BOT_USERNAME as bu, START_IMG
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
